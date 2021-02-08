5569e56da8549f9ef51d7de97a20f8c911a92bd5
Laszlo <uray077@gmail.com>
2021-02-08 15:54:44 +0000

    update to rank

---
+++ rank.py
@@ -0,0 +1,30 @@
+from string import ascii_lowercase
+
+def rank(st, we, n):
+    # your code
+    if not st:
+        return 'No participants'
+    print(len(list(st)))
+    if n > len(list(st)):
+        return 'Not enough participants'
+    name = ''
+    
+    LETTERS = {letter: str(index) for index, letter in enumerate(ascii_lowercase, start=1)} 
+
+    def alphabet_position(text):
+        text = text.lower()
+
+        numbers = [LETTERS[character] for character in text if character in LETTERS]
+
+        return ' '.join(numbers)
+
+    char = [chr(i) for i in range(ord('a'),ord('z')+1)]
+    nums = range(26)
+
+    return name
+
+
+rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4)# "Benjamin")
+rank("Lagon,Lily", [1, 5], 2)# "Lagon")
+rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8)# "Not enough participants")
+rank("", [4, 2, 1, 4, 3, 1, 2], 6)#"No participants")
--- second_index.py
+++ second_index.py
@@ -3,14 +3,18 @@ def second_index(text, symbol):
         returns the second index of a symbol in a given text
     """
     # your code here
-    index_of_second = None
-    print(text[text.find(symbol)+1:])
-    for i in text[text.find(symbol)+1:]:
-        print(i)
-        if i == symbol:
-            index_of_second = text.find(i)
-    
-    return index_of_second
+    #print(text[text.find(symbol)+1:])
+    #from_symbol = text[text.find(symbol)+1:]
+    '''
+    for i in range(len(from_symbol)):
+        print(from_symbol)
+        if from_symbol[i] == symbol:
+            index_of_second = text.find(from_symbol[i],text.find(from_symbol))
+    '''
+    index_of_second = text.find(symbol,text.find(symbol)+1)
+    if index_of_second < 1:
+        index_of_second = None
+    return index_of_second 
 
 second_index("sims", "s") # 3, "First"
 second_index("find the river", "e") # 12, "Second"
