b5a214858e66eadfdf1b8e13dd21ab538f4fd8e6
Laszlo <uray077@gmail.com>
2021-03-18 20:41:20 +0000

    order py

--- order.py
+++ order.py
@@ -1,11 +1,8 @@
 def order(sentence):
     new_string = [x for x in sentence.split()]
-    n = ''
-    for i in range(len(sentence)):
-        print(sentence[i])
-        if sentence[i].isnumeric:
-            n.join(sentence[i])
-    #return " ".join(sorted(new_string))
+    return " ".join(sorted(new_string, key=lambda x :int("".join([i for i in x if i.isdigit()]))))
+    # Alternative
+    #return ' '.join(sorted(words.split(), key=lambda   w:sorted(w)))
 
 
 order("is2 Thi1s T4est 3a")# "Thi1s is2 3a T4est")
